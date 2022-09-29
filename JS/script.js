a9951cd7a80794027a715523cacaeda2a02654dd
0cham0m1le0 <107765648+0cham0m1le0@users.noreply.github.com>
2022-09-30 01:57:52 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -28,7 +28,7 @@ document.querySelector('#add-task__cansel-button').addEventListener('click', () 
 document.querySelector('#add-task__add-button').addEventListener('click', () => {
 	if(!taskTitleInput.value) return;
 	init();
-	addScroll();
+	addScrollbar();
 });
 // Control by keyboard
 taskTitleInput.addEventListener('keydown', (e) => {
@@ -36,7 +36,7 @@ taskTitleInput.addEventListener('keydown', (e) => {
 	if(e.code === 'Enter'){
 		if(!taskTitleInput.value) return;
 		init();
-		addScroll();
+		addScrollbar();
 	}
 	
 	if(e.key === 'Escape'){
@@ -50,8 +50,10 @@ function createContent() {
 	const task = `
 	<div class="tasks__cell task">
 		<div class="task__content">
-			<input type="checkbox" class="task__checkbox" id="task-checkbox${taskCheckboxCount}">
-			<label for="task-checkbox${taskCheckboxCount}" class="task__title" id="task-title">${taskTitleInput.value}</label>
+			<div class="task__header">
+				<input type="checkbox" class="task__checkbox" id="task-checkbox${taskCheckboxCount}">
+				<label for="task-checkbox${taskCheckboxCount}" class="task__title" id="task-title">${taskTitleInput.value}</label>
+			</div>
 			<div class="task__description"> <br> ${taskDescriptionInput.value}</div>
 		</div>
 		<div class="task__managment">
@@ -76,17 +78,26 @@ function init() {
 	const fragment = document.createDocumentFragment();
 
 	fragment.appendChild(createContent());
-	appendContent(fragment);	
+	appendContent(fragment);
+
+	tasksCount++;
+	tasksLeft()
 }
 
-function addScroll(){
-	if(tasksCount >= 6){
-		main.classList.add('scroll');
+//* Scrollbar
+function addScrollbar(){
+
+	if(tasks.clientHeight >= 500){
+		main.classList.add('scroll');	
 	}
-	tasksCount++;
 
 	taskTitleInput.value = '';
 	taskDescriptionInput.value = '';
+}
+function removeScrollbar(){
+	if(tasks.clientHeight < 500){
+		main.classList.remove('scroll');
+	}
 }
 
 //* Highlight done task
@@ -94,7 +105,15 @@ window.addEventListener('click', function(e){
 	document.querySelectorAll('.task__checkbox').forEach(function(element){
 		if(e.target == element){
 			element.nextElementSibling.classList.toggle('done');
+
+			let allTasks = document.querySelectorAll('.task');
+			let doneTasks = document.querySelectorAll('.done');
+
+			let activeTasksLeft = allTasks.length - doneTasks.length;
+			tasksCount = activeTasksLeft;
+			tasksLeft();
 		}
+
 	})
 })
 
@@ -103,14 +122,111 @@ window.addEventListener('click', function(e){
 	document.querySelectorAll('.fa-regular').forEach(function(element){
 		if(e.target == element){
 			element.closest('div.task').remove();
+
+			if(element.parentElement.parentElement.previousElementSibling.firstElementChild.
+				lastElementChild.classList.contains('done')){
+					tasksLeft()
+			}
+			else{
+				tasksCount--;
+				tasksLeft()
+			}
+		}
+	})
+})
+document.querySelector('#clear-button').addEventListener('click', () => {
+	document.querySelectorAll('.done').forEach(el => {
+		el.closest('div.task').remove();
+
+		removeScrollbar()
+
+		tasksLeft()
+	})
+})
+
+//* Tasks left
+let footerTasksLeft = document.querySelector('#tasks-left');
+function tasksLeft(){
+	if(tasksCount == 1){
+		footerTasksLeft.innerHTML = `${tasksCount} item left`;
+	}
+	else{
+		footerTasksLeft.innerHTML = `${tasksCount} items left`;
+	}
+}
+tasksLeft();
+
+
+//* Sort category
+// Visual part
+const sortCategories = document.querySelectorAll('#sort-categoty');
+sortCategories[0].classList.add('active');
+document.querySelector('#sort-list').addEventListener('click', (e) => {
+	sortCategories.forEach(category => {
+		if(category.classList.contains('active')){
+			category.classList.remove('active');
+		}
+		if(e.target == category){
+			category.classList.add('active');
 		}
 	})
 })
 
-document.querySelector('#clear-button').addEventListener('click', () => {
-	document.querySelectorAll('.done').forEach(el => {
-		el.closest('div.task').remove();
-	})
+// Actually sorting
+window.addEventListener('click', (e) => {
+
+	let tasks = document.querySelectorAll('.task');
+
+	if(e.target == sortCategories[0]){
+
+		addButton.style.display = 'block';
+
+		tasks.forEach(task => {
+			task.closest('div.task').style.display = 'flex';
+		})
+	}
+
+	else if(e.target == sortCategories[1]){
+
+		addButton.style.display = 'none';
+
+		tasks.forEach(task => {
+			if(task.firstElementChild.firstElementChild.lastElementChild.classList.contains('done')){
+				task.closest('div.task').style.display = 'none';
+			}
+			if(!task.firstElementChild.firstElementChild.lastElementChild.classList.contains('done')){
+				task.closest('div.task').style.display = 'flex';
+			}			
+		})
+	}
+
+	else if(e.target == sortCategories[2]){
+
+		addButton.style.display = 'none';
+
+		tasks.forEach(task => {
+			if(!task.firstElementChild.firstElementChild.lastElementChild.classList.contains('done')){
+				task.closest('div.task').style.display = 'none';
+			}
+			if(task.firstElementChild.firstElementChild.lastElementChild.classList.contains('done')){
+				task.closest('div.task').style.display = 'flex';
+			}
+		})
+	}
+
+	if(sortCategories[1].classList.contains('active')){
+		tasks.forEach(task => {
+			if(e.target == task.firstElementChild.firstElementChild.lastElementChild){
+				task.style.display = 'none';
+			}
+		})
+	}
+
+	if(sortCategories[2].classList.contains('active')){
+		tasks.forEach(task => {
+			if(e.target == task.firstElementChild.firstElementChild.lastElementChild){
+				task.style.display = 'none';
+			}
+		})
+	}
 })
-
-
